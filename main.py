a9a13fe865ddfb04fda82bc8fcdd73ae11dae5a5
Tien Nguyen <52200088@student.tdtu.edu.vn>
2024-12-14 23:19:23 +0700

    first

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException, Form
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Gắn cứng thông tin tài khoản
+VALID_USERNAME = "admin"
+VALID_PASSWORD = "12345"
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+@app.post("/login")
+def login(username: str = Form(...), password: str = Form(...)):
+    if username != VALID_USERNAME:
+        raise HTTPException(status_code=401, detail="Sai tên đăng nhập")
+    if password != VALID_PASSWORD:
+        raise HTTPException(status_code=401, detail="Sai mật khẩu")
+    return {"message": "Bạn đã đăng nhập thành công"}
