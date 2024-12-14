c1689e18bb15fcdb733e3aadd1947d43f36353fc
TDeV <thanhtien.z8436@gmail.com>
2024-12-15 01:36:17 +0700

    Loi chua tao bang

--- crud.py
+++ crud.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from models import User
 
 # Khai báo Base class cho SQLAlchemy
 Base = declarative_base()
@@ -24,3 +26,13 @@ Base.metadata.create_all(bind=engine)
 
 # Tạo SessionLocal để tương tác với cơ sở dữ liệu
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+def create_user(db: Session, displayname: str, username: str, hashed_password: str):
+    db_user = User(displayname=displayname, username=username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
