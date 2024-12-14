bd587983b8b2cc293222ad441ede4c4aca07a284
TDeV <thanhtien.z8436@gmail.com>
2024-12-15 00:02:44 +0700

    CORS

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, HTTPException, Form
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Gắn cứng thông tin tài khoản
 VALID_USERNAME = "admin"
